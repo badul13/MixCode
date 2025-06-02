b087d3cbc035c683c95a91a2bbdad4723016be74
parksungja <parksungja@gmail.com>
2025-06-02 22:28:43 +0900

    token -> accessToken

--- Front/src/api/axiosInstance.js
+++ Front/src/api/axiosInstance.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = 'https://4dea-210-119-104-214.ngrok-free.app';
+const baseURL = "https://4dea-210-119-104-214.ngrok-free.app";
 
 const axiosInstance = axios.create({ baseURL });
 
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("accessToken");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
